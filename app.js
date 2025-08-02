a4298266156eebc33064a3fb5c4fe6e489507ad2
biancapcr <biancacristina.pocora@gmail.com>
2025-08-02 18:51:41 +0200

    [add]  endpoint and endpoint listen

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ const express = require ('express');
 //funzione per creare l'oggetto express
 const app = express();
 //definire la porta
-const port = 3000
+const port = 3000;
+
+//definire rotta principale
+app.get('/', (req, res) => {
+  res.send('Server del mio blog');
+});
+
+app.listen(port, () => {
+  console.log('Server in ascolto alla porta ${port}');
+});
