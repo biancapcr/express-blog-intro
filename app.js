8e31520f76d3d022a1776242de959e01511998c7
biancapcr <biancacristina.pocora@gmail.com>
2025-08-02 18:44:00 +0200

    [add] import express [add] function and [add] setting up port

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+//console.log("Hello World");
+//importo express
+const express = require ('express');
+//funzione per creare l'oggetto express
+const app = express();
+//definire la porta
+const port = 3000
